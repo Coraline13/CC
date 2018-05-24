8fc48f03c9422048dd41986f7fa666672b8ebf18
Coraline <coralia.bodea@gmail.com>
2018-05-24 23:01:23 +0300

    Add shortestWay method

--- src/Lab1.java
+++ src/Lab1.java
@@ -4,39 +4,62 @@ import java.lang.*;
 class Attempt {
     private String currentState;
     private ArrayList<String[]> way;
+    private static Map<String[], String[]> transitions;
+    private static String[] boat = {"M", "MW", "MG", "MC"};
+    private static String[][][] map =
+            {
+                    {{"MWGC", "MG"}, {"WC", "MG"}},
+                    {{"WC", "M"}, {"MWC", "G"}},
+                    {{"MWC", "MC"}, {"W", "MGC"}},
+                    {{"MWC", "MW"}, {"C", "MWG"}},
+                    {{"W", "MG"}, {"MWG", "C"}},
+                    {{"C", "MG"}, {"MGC", "W"}},
+                    {{"MWG", "MW"}, {"G", "MWC"}},
+                    {{"MGC", "MC"}, {"G", "MWC"}},
+                    {{"G", "M"}, {"MG", "WC"}},
+                    {{"MG", "MG"}, {".", "MWGC"}}
+            };
 
-    static {
-        String[][][] map =
-                {
-                        {{"MWGC", "MG"}, {"WC", "MG"}},
-                        {{"WC", "M"}, {"MWC", "G"}},
-                        {{"MWC", "MC"}, {"W", "MGC"}},
-                        {{"MWC", "MW"}, {"C", "MWG"}},
-                        {{"W", "MG"}, {"MWG", "C"}},
-                        {{"C", "MG"}, {"MGC", "W"}},
-                        {{"MWG", "MW"}, {"G", "MWC"}},
-                        {{"MGC", "MC"}, {"G", "MWC"}},
-                        {{"G", "M"}, {"MG", "WC"}},
-                        {{"MG", "MG"}, {".", "MWGC"}}
-                };
+    private static Map<String[], String[]> createMap() {
         Map<String[], String[]> transitions = new HashMap<>();
         for (String[][] m : map) {
             transitions.put(m[0], m[1]);
         }
+        return transitions;
     }
 
     public Attempt(String initialState) {
         currentState = initialState;
         way = new ArrayList<>();
+        transitions = createMap();
     }
 
-//    public ArrayList shortestWay() {
-//
-//    }
+    private boolean validTransition(String[] current_key) {
+        return transitions.containsKey(current_key);
+    }
+
+    private void shortestWay() {
+        for (String b : boat) {
+            String[] key = {currentState, b};
+
+            if (validTransition(key)) {
+                String[] value = {transitions.get(key)};
+                way.add(key[1]);
+                way.add(value[0]);
+                way.add(value[1]);
+
+                if(currentState.equals(".")){
+                    return;
+                }
+
+                this.shortestWay();
+            }
+        }
+    }
 
     public void printWay() {
         int counter = 1;
-        //this.shortestWay();
+        this.shortestWay();
 
         System.out.print("state 0: MWGC --------- .");
 
