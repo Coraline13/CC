3964e532edb40ac859c7b9fff20325ebaf61ffa4
Coraline <coralia.bodea@gmail.com>
2018-06-04 05:10:45 +0300

    Add Lab4

---
+++ src/Lab4/Lab4.java
@@ -0,0 +1,15 @@
+//The aim of this programming assigment is to write a program that given a regular language described by
+//a regular expression determines the corresponding non-deterministic finite automaton that recognizes it.
+//The simulation will specify all the transitions starting from the initial state and arriving to the accept state.
+
+package Lab4;
+
+import javax.swing.*;
+import java.lang.*;
+
+public class Lab4 {
+    public static void main(String[] args) {
+//        TuringMachine attempt = new TuringMachine();
+//        attempt.showMessage();
+    }
+}
