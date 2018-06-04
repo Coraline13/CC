d0555e9cb0da4fc66d57fba94402d4b4b68c4711
Coraline <coralia.bodea@gmail.com>
2018-06-04 04:21:41 +0300

    Finish Lab8

--- src/Lab8/Lab8.java
+++ src/Lab8/Lab8.java
@@ -4,14 +4,7 @@
 package Lab8;
 
 import javax.swing.*;
-import java.util.*;
 import java.lang.*;
-
-import javafx.util.Pair;
-
-enum Alphabet {
-
-}
 
 enum Move {
     LEFT, RIGHT
@@ -20,55 +13,143 @@ enum Move {
 enum States {
     STATE0, STATE1, STATE2, STATE3, STATE4, STATE5, STATE6, STATE7, STATE8, STATE9, ACCEPT, REJECT;
 
-    public Pair<Character, Move> transition(Character currentSymbol) {
+    public ReturnTuple transition(Character currentSymbol) {
         switch (this) {
             case STATE0:
-                if (currentSymbol.equals('a')){
-                    return new Pair<>(, Move.RIGHT);
+                if (currentSymbol.equals('a')) {
+                    return new ReturnTuple('x', Move.RIGHT, STATE1);
                 }
+                break;
             case STATE1:
-                return true;
+                if (currentSymbol.equals('a')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE1);
+                } else if (currentSymbol.equals('b')) {
+                    return new ReturnTuple('y', Move.RIGHT, STATE2);
+                }
+                break;
             case STATE2:
-                return false;
+                if (currentSymbol.equals('b')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE2);
+                } else if (currentSymbol.equals('c')) {
+                    return new ReturnTuple('z', Move.RIGHT, STATE3);
+                }
+                break;
             case STATE3:
-                return true;
+                if (currentSymbol.equals('x')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE4);
+                } else {
+                    return new ReturnTuple(currentSymbol, Move.LEFT, STATE3);
+                }
             case STATE4:
-                return false;
+                if (currentSymbol.equals('a')) {
+                    return new ReturnTuple('x', Move.RIGHT, STATE6);
+                } else if (currentSymbol.equals('y')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE5);
+                }
+                break;
             case STATE5:
-                return false;
+                if (currentSymbol.equals(' ')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, ACCEPT);
+                } else if (!currentSymbol.equals('b') && !currentSymbol.equals('c')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE5);
+                }
+                break;
             case STATE6:
-                return true;
+                if (currentSymbol.equals('y')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE7);
+                } else if (currentSymbol.equals(' ')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, REJECT);
+                } else {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE6);
+                }
             case STATE7:
-                return false;
+                if (currentSymbol.equals('b')) {
+                    return new ReturnTuple('y', Move.RIGHT, STATE8);
+                } else if (currentSymbol.equals('y')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE7);
+                }
+                break;
             case STATE8:
-                return true;
+                if (currentSymbol.equals('z')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE9);
+                } else if (currentSymbol.equals(' ')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, REJECT);
+                } else {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE8);
+                }
             case STATE9:
-                return false;
-            case ACCEPT:
-                return falsa;
-            case REJECT:
-                ceva;
+                if (currentSymbol.equals('c')) {
+                    return new ReturnTuple('z', Move.RIGHT, STATE3);
+                } else if (currentSymbol.equals('z')) {
+                    return new ReturnTuple(currentSymbol, Move.RIGHT, STATE9);
+                }
+                break;
+            default:
+                throw new IllegalStateException();
         }
+        return new ReturnTuple(currentSymbol, Move.RIGHT, REJECT);
+    }
+}
+
+class ReturnTuple {
+    private Character sign;
+    private Move direction;
+    private States state;
+
+    public ReturnTuple(Character sign, Move direction, States state) {
+        this.sign = sign;
+        this.direction = direction;
+        this.state = state;
+    }
+
+    public Character getSign() {
+        return sign;
+    }
+
+    public Move getDirection() {
+        return direction;
+    }
+
+    public States getState() {
+        return state;
     }
 }
 
 class TuringMachine {
     String input;
-    Character currentSymbol;
+    States currentState;
+    int index;
     boolean validInput;
 
     /**
      * constructor
      */
     public TuringMachine() {
-
+        currentState = States.STATE0;
+        index = 1;
     }
 
     // returns true if the given word is accepted by the automaton
-//    public boolean isAccepted() {
-//        Character c = input.charAt(0);
-//
-//    }
+    public boolean isAccepted() {
+        ReturnTuple nextState = currentState.transition(input.charAt(index));
+
+        if (nextState.getState().equals(States.ACCEPT)) {
+            return true;
+        } else if (nextState.getState().equals(States.REJECT)) {
+            return false;
+        }
+
+        input = input.substring(0, index) + nextState.getSign() + input.substring(index + 1);
+        if (nextState.getDirection().equals(Move.LEFT)) {
+            index--;
+        } else if (nextState.getDirection().equals(Move.RIGHT)) {
+            index++;
+        }
+
+        currentState = nextState.getState();
+
+        return isAccepted();
+    }
 
     // GUI
     public void showMessage() {
@@ -83,6 +164,7 @@ class TuringMachine {
         if (input == null) {
             return;
         }
+        input = " " + input + " ";
 
         validInput = isAccepted();
 
